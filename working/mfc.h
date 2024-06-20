8c5cf20b5cbde2498a273bfd5c89a06a10f9aff9
shensj <3207180297@qq.com>
2024-06-20 17:43:40 +0800

    mfc路径

---
+++ working/mfc.h
@@ -0,0 +1,30 @@
+
+// class CDC{};
+// class CRect{
+//     public:
+//     int Height(){return 0;}};
+
+// #define afx_msg
+// #define BOOL bool
+// #define UINT int
+
+// class CMarkNote{};
+// class CScaleMap{};
+// class CPoint3D{};
+// class CPoint{};
+// class CDataExchange{};
+// class CString{};
+// #define COLORREF int
+// class CStringArray{ 
+//     int GetSize(){return 0;}
+//     void Add(CString){}
+//     };
+// //template<Template T> class CSSPtrArray{};
+// class CBrush{};
+
+#define WIN32
+#define _DEBUG
+#define _WINDOWS
+#define _WINDLL
+#define _MBCS
+#define _AFXEXT
